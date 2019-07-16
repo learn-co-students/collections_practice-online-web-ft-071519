79a3ba84922929fd02a641bdf8794b2e1dd7ba49
Audrea Cook <audreacwork@gmail.com>
2019-07-16 14:22:43 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def sort_array_asc(arr)
   arr.sort
 end
@@ -12,3 +14,27 @@ def sort_array_char_count(arr)
   end
 end
   
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  arr
+end
+
+def swap_elements_from_to(arr, index, destination_index)
+  arr[index], arr[destination_index] = arr[destination_index], arr[index]
+  arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  # iterate through array using map
+  arr.map do |element|
+    # turn element string into array
+    element_arr =  element.split('')
+      element_arr[2] = "$"
+    # turn element array back into string
+    element = element_arr.join('')
+  end
+end
